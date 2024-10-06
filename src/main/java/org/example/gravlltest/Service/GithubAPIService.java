d1c6846546fd410e76768b6856696934d38a434c
PrethamMuthappa <prethambachira1819@gmail.com>
2024-10-06 12:30:18 +0530

    Fetch commits

--- src/main/java/org/example/gravlltest/Service/GithubAPIService.java
+++ src/main/java/org/example/gravlltest/Service/GithubAPIService.java
@@ -1,5 +1,6 @@
 package org.example.gravlltest.Service;
 
+import org.example.gravlltest.Model.CommitResponse;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -7,6 +8,10 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 @Service
@@ -21,13 +26,32 @@ public class GithubAPIService {
         this.restTemplate = restTemplate;
     }
 
-    public Object FetchCommits() {
+    public List<CommitResponse> FetchCommits() {
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Accept","application/vnd.github.v3+json");
-        ResponseEntity responseEntity = restTemplate.exchange("", HttpMethod.GET, new HttpEntity<>(headers), new ParameterizedTypeReference<Object>() {
+        String commitsUrl = String.format("%s/repos/%s/%s/commits?per_page=5", baseurl, owner, repo);
+        ResponseEntity<List<Map<String,Object>>> responseEntity = restTemplate.exchange(
+                commitsUrl,
+                HttpMethod.GET,
+                new HttpEntity<>(headers),
+                new ParameterizedTypeReference<List<Map<String, Object>>>() {
+                }
+        );
+        List<CommitResponse>commitResponses=new ArrayList<>();
 
-        });
-        return responseEntity.getBody();
+        if(responseEntity.getBody()!=null){
+            for(Map<String,Object> commit: responseEntity.getBody()){
+                String sha= (String) commit.get("sha");
+                CommitResponse responsedetails=fetchcommitDetails(sha);
+                if(responsedetails!=null){
+                    commitResponses.add(responsedetails);
+                }
+            }
+        }
+     return commitResponses;
+    }
+
+    private CommitResponse fetchcommitDetails(String sha) {
     }
 }
